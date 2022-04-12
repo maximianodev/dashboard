a3d21c35e3639a92c2e2323c796f02770528d96d
Lucas Maximiano <lucas97lopes@hotmail.com>
2022-04-12 18:09:24 -0300

    feat: react hook form

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -1,39 +1,43 @@
-import React from 'react'
+import React, { forwardRef, ForwardRefRenderFunction } from "react";
 import {
   FormControl,
+  FormErrorMessage,
   FormLabel,
   Input as ChakraInput,
-  InputProps as ChakraInputProps
+  InputProps as ChakraInputProps,
 } from "@chakra-ui/react";
+import { FieldError } from "react-hook-form";
 
 interface InputProps extends ChakraInputProps {
   name: string;
   label?: string;
+  error?: FieldError;
 }
 
-export const Input = ({ name, label, ...rest }: InputProps) => {
+const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> = (
+  { name, label, error = null, ...rest },
+  ref
+) => {
   return (
-    <FormControl>
-      {!!label &&
-        <FormLabel
-          htmlFor={name}
-        >
-          {label}
-        </FormLabel>}
+    <FormControl isInvalid={!!error}>
+      {!!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
 
       <ChakraInput
         name={name}
         id={name}
-        focusBorderColor='pink.500'
-        bgColor='gray.900'
-        variant='filled'
-        size='lg'
+        focusBorderColor="pink.500"
+        bgColor="gray.900"
+        variant="filled"
+        size="lg"
         _hover={{
-          bgColor: 'gray.900'
+          bgColor: "gray.900",
         }}
+        ref={ref}
         {...rest}
       />
+      {!!error && <FormErrorMessage>{error.message}</FormErrorMessage>}
     </FormControl>
-  )
-}
+  );
+};
 
+export const Input = forwardRef(InputBase);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,51 +1,79 @@
 import { Button, Flex, Stack } from "@chakra-ui/react";
-import { useForm } from "react-hook-form";
+import { SubmitHandler, useForm, useFormState } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
 import { Input } from "../components/Form/Input";
 
-export default function SignIn() {
-  const { register, handleSubmit, formState: { errors } } = useForm();
+type SignInFormData = {
+  email: string;
+  password: string;
+};
 
-  function handleSignIn() { }
+const signInFormSchema = yup
+  .object()
+  .shape({
+    email: yup.string().required("E-mail obrigatório").email("E-mail inválido"),
+    senha: yup.string().required(),
+  })
+  .required();
+
+export default function SignIn() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm({
+    resolver: yupResolver(signInFormSchema)
+  });
+
+  const handleSignIn: SubmitHandler<SignInFormData> = async (values) => {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+
+    console.log(
+      "🚀 ~ file: index.tsx ~ line 9 ~ handleSignIn ~ values",
+      values
+    );
+  };
 
   return (
-    <Flex
-      w='100vw'
-      h='100vh'
-      align='center'
-      justify='center'
-    >
+    <Flex w="100vw" h="100vh" align="center" justify="center">
       <Flex
-        as='form'
-        w='100%'
+        as="form"
+        w="100%"
         maxW={360}
-        bg='gray.800'
-        p='8'
+        bg="gray.800"
+        p="8"
         borderRadius={8}
-        flexDir='column'
+        flexDir="column"
+        onSubmit={handleSubmit(handleSignIn)}
       >
         <Stack spacing={4}>
           <Input
-            type='email'
-            name='email'
-            label='E-mail'
+            type="email"
+            name="email"
+            label="E-mail"
+            error={errors.email}
             {...register("email")}
           />
           <Input
-            type='password'
-            name='password'
-            label='Password'
+            type="password"
+            name="password"
+            label="Password"
+            error={errors.password}
             {...register("password")}
           />
         </Stack>
         <Button
-          type='submit'
-          mt='6'
-          colorScheme='pink'
-          size='lg'
+          type="submit"
+          mt="6"
+          colorScheme="pink"
+          size="lg"
+          isLoading={isSubmitting}
         >
           Entrar
         </Button>
       </Flex>
     </Flex>
-  )
+  );
 }
